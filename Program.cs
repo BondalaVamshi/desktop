d5d084c7597b0f2e9fb0d55b47a63beddb2afe90
vamshi <vamshibondala888@gmail.com>
2021-04-05 16:21:33 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is hello");
+            Console.WriteLine("this is hello");
+
+        }
+    }
+}
